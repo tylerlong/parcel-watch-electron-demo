51865a1ee62e95ff50db7a1d2ab067de817268ef
Tyler Liu <tyler4long@gmail.com>
2023-03-19 10:14:44 -0700

    Add electron

---
+++ src/electron.ts
@@ -0,0 +1,32 @@
+// Modules to control application life and create native browser window
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, '..', 'build', 'preload.js'),
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:1234');
+
+  mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
