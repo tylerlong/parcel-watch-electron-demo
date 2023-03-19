8da7ec8fb9e387ae874aa6559a8ec7e495c288bf
Tyler Liu <tyler4long@gmail.com>
2023-03-19 15:42:56 -0700

    Simplify config

--- scripts/watch.ts
+++ scripts/watch.ts
@@ -3,7 +3,7 @@ import { run } from 'shell-commands';
 const main = async () => {
   await run('rm -rf build');
   run('parcel src/index.html --dist-dir build -p 1234');
-  run('parcel watch --target electron --target preload -p 1235');
+  run('parcel watch --target electron -p 1235');
 };
 
 main();
