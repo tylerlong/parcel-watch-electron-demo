4876b00155c285e19936d63f098e1281ff15803e
Tyler Liu <tyler4long@gmail.com>
2023-03-26 17:04:12 -0700

    auto rebuild preload

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ So that, whenever you change anything in the renderer process, it will hot reloa
 
 What's more, whenever you change the electron main process code, it will restart your electron app.
 
-但是，preload 的代码没办法动态更新。 甚至不能用 parcel watch. 只能用parcel build。
+但是，preload 的代码没办法动态更新。 甚至不能用 parcel watch. 只能用 parcel build。
 不过好在 preload 的代码都很简单。
--- scripts/watch.ts
+++ scripts/watch.ts
@@ -2,7 +2,7 @@ import { run } from 'shell-commands';
 
 const main = async () => {
   await run('rm -rf build');
-  await run('parcel build --target preload');
+  run("nodemon --watch src/preload.ts --exec 'parcel build --target preload'");
   run('parcel src/index.html --dist-dir build -p 1234');
   run('parcel watch --target electron -p 1235');
 };
